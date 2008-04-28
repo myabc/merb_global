4616c5a68f9a686a3be69a6eef0798b42cb744e0
Maciej Piechotka <uzytkownik2@gmail.com>
2008-04-28 21:18:25 +0200

    Added gettext support

--- lib/merb_global/providers.rb
+++ lib/merb_global/providers.rb
@@ -1,5 +1,6 @@
 require 'merb_global/providers/activerecrd'
 require 'merb_global/providers/datamapper'
+require 'merb_global/providers/gettext'
 require 'merb_global/providers/mock'
 require 'merb_global/providers/sequel'
 require 'merb_global/providers/yaml'
---
+++ lib/merb_global/providers/gettext.rb
@@ -0,0 +1,28 @@
+require 'gettext'
+
+# I'm not sure if it is the correct way of doing it.
+# As far it seems to be simpler.
+class Thread
+  def gettext_context #:nodoc:
+    @gettext_context ||= Merb::Global::Providers::Gettext::GettextContext.new
+  end
+end
+
+module Merb
+  module Global
+    module Providers
+      class Gettext #:nodoc:
+        def translate_to singular, plural, opts
+          context = Thread.current.gettext_context
+          context.locale = Locale::Object.new opts[:lang]
+          context.ngettext(singular, plural, opts[:n])
+        end
+        class GettextContext
+          include GetText
+          # Please change it to proper location
+          bindtextdomian "merbapp", File.join(Merb.root, 'app', 'locale')
+        end
+      end
+    end
+  end
+end
