0fc7cc170f91e8aa150b8148b2c4fee97d2a293a
Maciej Piechotka <uzytkownik2@gmail.com>
2008-04-28 21:38:55 +0200

    Changed provider setup

--- lib/merb_global/providers.rb
+++ lib/merb_global/providers.rb
@@ -1,12 +1,17 @@
-require 'merb_global/providers/activerecrd'
-require 'merb_global/providers/datamapper'
-require 'merb_global/providers/mock'
-require 'merb_global/providers/sequel'
-require 'merb_global/providers/yaml'
-
 module Merb
   module Global
     module Providers
+      def self.provider
+        # provider = 'gettext'
+        provider = 'mock'
+        unless Merb::Plugins.config[:merb_global].nil?
+          unless Merb::Plugins.config[:merb_global][:provider].nil?
+            provider = Merb::Plugins.config[:merb_global][:provider]
+          end
+        end
+        require 'merb_global/providers/' + provider
+        eval "Merb::Global::Providers::#{provider.capitalize}.new"
+      end
     end
   end
 end
