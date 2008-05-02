4ffee456936b1d7c39daa43d67ae4aa10ca3cdb8
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-02 21:26:16 +0200

    Experimental support for controllers. Next stage - tests, tests and tests

--- lib/merb_global/controller.rb
+++ lib/merb_global/controller.rb
@@ -4,7 +4,25 @@ module Merb
   class Controller #:nodoc:
     include Merb::Global
     before do
-      # TODO: Language negotation
+      accept_language = self.env['HTTP_ACCEPT_LANGUAGE'].split(',')
+      accept_language.collect! {|lang| lang.delete " " "\n" "\r" "\t"}
+      accept_language.reject! {|lang| lang.empty?}
+      accept_language.collect! {|lang| lang.split ';'}
+      accept_language.collect! do |lang|
+        if lang.is_a? String
+          [lang, 1.0]
+        else
+          [lang[0], lang[1].to_f]
+        end
+      end
+      accept_language.sort! {|lang_a, lang_b| lang_a[1] <=> lang_b[1]}
+      until accept_language.empty?
+        clang = accept_language.pop[0]
+        if self.provider.support? clang
+          self.lang = clang
+          break
+        end
+      end
     end
   end
 end
