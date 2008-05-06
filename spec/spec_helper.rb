2f62aec059aa700b975ec88e19b574a34bd28116
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-06 02:06:12 +0200

    Updated specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
 $TESTING=true
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
-require 'merb_global/base'
+require 'merb-core'
+require 'merb_global'
+
+Spec::Runner.configure do |config|
+  config.include(Merb::Test::ControllerHelper)
+end
