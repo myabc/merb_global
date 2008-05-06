9608e5a3d7b53b58ce20bd902dddf28f3fef9a6e
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-06 21:46:31 +0200

    Updates to specs

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -7,7 +7,17 @@ class Test < Merb::Controller
 end
 
 describe Merb::Controller do
-  it "should set the language on request" do
+  it "should set language to english by default" do
     controller = dispatch_to(Test, :index)
+    controller.lang.should == "en"
+  end
+  it "should set language according to the preferences" do
+    env = {'HTTP_ACCEPT_LANGUAGE' => 'fr'}
+    controller = dispatch_to(Test, :index, env) do |controller|
+      provider = controller.provider = mock('provider')
+      provider.should_receive(:supported?).and_return(true)
+    end
+    controller.lang.should == "fr"
   end
 end
+
