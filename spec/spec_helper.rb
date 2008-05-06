60792b6aa0b5e2a367a7d450c9a3cdcf4201c43d
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-06 21:03:43 +0200

    Start merb evn on specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@ $TESTING=true
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 require 'merb-core'
 require 'merb_global'
+require 'spec'
+
+Merb.start_environment(:testing => true, :adapter => 'runner', :environment => ENV['MERB_ENV'] || 'test')
 
 Spec::Runner.configure do |config|
   config.include(Merb::Test::ControllerHelper)
