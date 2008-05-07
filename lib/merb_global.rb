0fc5ea61ba7f9e7c5c5382bb3ebdd10eba24679e
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-07 21:52:13 +0200

    Support for rake

--- lib/merb_global.rb
+++ lib/merb_global.rb
@@ -1,2 +1,4 @@
 require 'merb_global/base'
 require 'merb_global/controller'
+
+Merb::Plugins.add_rakefiles 'merb_global/rake'
---
+++ lib/merb_global/rake.rb
@@ -0,0 +1,6 @@
+namespace :merb_global do
+  desc "create migration"
+  task :migration do
+    puts "Unimplemented yet"
+  end
+end
