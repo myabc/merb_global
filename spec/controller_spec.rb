29a5a2d9574edf6f69196c67deb363d5f7021ecd
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-10 22:50:32 +0200

    Some cleaning of specs

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -16,8 +16,7 @@ describe Merb::Controller do
   it "should set language according to the preferences" do
     controller = dispatch_to(TestController, :index) do |controller|
       controller.request.env['HTTP_ACCEPT_LANGUAGE'] = 'fr'
-      controller.provider = provider = mock
-      provider.expects(:supported?).once.returns(true)
+      controller.provider = provider = stub(:supported? => true)
     end
     controller.lang.should == 'fr'
   end
@@ -25,16 +24,18 @@ describe Merb::Controller do
     controller = dispatch_to(TestController, :index) do |controller|
       controller.request.env['HTTP_ACCEPT_LANGUAGE'] =
         'de;q=0.8,en;q=1.0,es;q=0.6'
-      controller.provider = provider = mock
-      provider.expects(:supported?).times(3).returns(true, true, false)
+      controller.provider = mock "provider" do |provider|
+        provider.expects(:supported?).with("de").returns(true)
+        provider.expects(:supported?).with("en").returns(false)
+        provider.stubs(:supported?).with("es").returns(true)
+      end
     end
     controller.lang.should == 'de'
   end
   it "should assume 1.0 as default weight" do
     controller = dispatch_to(TestController, :index) do |controller|
       controller.request.env['HTTP_ACCEPT_LANGUAGE'] = 'it,en;q=0.7'
-      provider = controller.provider = mock
-      provider.expects(:supported?).times(2).returns(true)
+      provider = controller.provider = stub(:supported? => true)
     end
     controller.lang.should == 'it'
   end
