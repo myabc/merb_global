0169c88b54cea7f69e39f42806b0d62a63bd0dd3
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-10 20:59:56 +0200

    Changed name of controller to allow many 'Test' classes

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -1,6 +1,6 @@
 require File.join(File.dirname(__FILE__), 'spec_helper.rb')
 
-class Test < Merb::Controller
+class TestController < Merb::Controller
   def index
     "index"
   end
@@ -8,13 +8,13 @@ end
 
 describe Merb::Controller do
   it "should set language to english by default" do
-    controller = dispatch_to(Test, :index) do |controller|
+    controller = dispatch_to(TestController, :index) do |controller|
       controller.request.env.delete 'HTTP_ACCEPT_LANGUAGE'
     end
     controller.lang.should == 'en'
   end
   it "should set language according to the preferences" do
-    controller = dispatch_to(Test, :index) do |controller|
+    controller = dispatch_to(TestController, :index) do |controller|
       controller.request.env['HTTP_ACCEPT_LANGUAGE'] = 'fr'
       provider = controller.provider = mock('provider')
       provider.stub!(:supported?).and_return(true)
@@ -22,7 +22,7 @@ describe Merb::Controller do
     controller.lang.should == 'fr'
   end
   it "should take the weights into account" do
-    controller = dispatch_to(Test, :index) do |controller|
+    controller = dispatch_to(TestController, :index) do |controller|
       controller.request.env['HTTP_ACCEPT_LANGUAGE'] =
         'de;q=0.8,en;q=1.0,es;q=0.6'
       provider = controller.provider = mock('provider')
@@ -31,7 +31,7 @@ describe Merb::Controller do
     controller.lang.should == 'de'
   end
   it "should assume 1.0 as default weight" do
-    controller = dispatch_to(Test, :index) do |controller|
+    controller = dispatch_to(TestController, :index) do |controller|
       controller.request.env['HTTP_ACCEPT_LANGUAGE'] = 'it,en;q=0.7'
       provider = controller.provider = mock('provider')
       provider.stub!(:supported?).and_return(true)
