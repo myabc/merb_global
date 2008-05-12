5f1a780a48c9b501802ddc3777b0f986adb481b0
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-12 23:55:45 +0200

    Added mock provider specs

---
+++ spec/providers/mock_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Merb::Global::Providers::Mock do
+  before do
+    @provider = Merb::Global::Providers::Mock.new
+  end
+  describe '.translate_to' do
+    it 'should return plural for n > 1' do
+      @provider.translate_to("test", "tests", :n => 2).should == "tests"
+    end
+    it 'should return singular for n <= 1' do
+      @provider.translate_to("test", "tests", :n => 0).should == "test"
+      @provider.translate_to("test", "tests", :n => 1).should == "test"
+    end
+  end
+  describe '.supported?' do
+    it 'should return true' do
+      @provider.supported?(mock) == true
+    end
+  end
+  describe '.create!' do
+    it 'should do nothing' do
+      @provider.create!.should be_nil
+    end
+  end
+end
