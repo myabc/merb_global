332c6269401f8d702da382adc1b15e86ab73e0ae
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-12 23:31:04 +0200

    Updated spec

--- spec/providers/yaml_spec.rb
+++ spec/providers/yaml_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 require 'merb_global/providers/yaml'
 
+class Merb::Global::Providers::Yaml
+  attr_reader :lang
+end
+
 describe Merb::Global::Providers::Yaml do
   describe '._supported?' do
     it 'should return true if file exists' do
@@ -26,6 +30,14 @@ describe Merb::Global::Providers::Yaml do
       YAML.expects(:load_file).with(yaml_file).returns(nil)
       provider.translate_to "test", "tests", :lang => 'en', :n => 1
     end
+    it 'should mark nil if file do not exists' do 
+      provider = Merb::Global::Providers::Yaml.new
+      yaml_file = File.join Merb.root, 'app', 'locale', 'en.yaml'
+      File.expects(:exist?).with(yaml_file).returns(false)
+      provider.translate_to "test", "tests", :lang => 'en', :n => 1
+      provider.lang.should include("en")
+      provider.lang["en"].should be_nil
+    end
     it 'should check appropiete form' do
       provider = Merb::Global::Providers::Yaml.new
       yaml_file = File.join Merb.root, 'app', 'locale', 'en.yaml'
@@ -42,6 +54,14 @@ describe Merb::Global::Providers::Yaml do
       Merb::Global::Plural.expects(:which_form).with(*expected).returns(1)
       translated = provider.translate_to "test", "test", :lang => 'en', :n => 1
       translated.should == "tests"
+    end
+  end
+  describe '.create!' do
+    it "should create app/locale firectory" do
+      provider = Merb::Global::Providers::Yaml.new
+      file = File.join Merb.root, 'app', 'locale'
+      File.expects(:mkdirs).with(file).returns([file])
+      provider.create!
     end
   end
 end
