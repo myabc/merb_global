5ebc4687d95c961f5095e7634664faaddd63edca
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-17 12:50:19 +0200

    Updated Provider spec. Reached 100% C0 coverage.

--- spec/providers_spec.rb
+++ spec/providers_spec.rb
@@ -1,6 +1,65 @@
 require 'spec_helper'
 
+module Merb::Global::Providers
+  def self.provider_name
+    @@provider_name
+  end
+  def self.provider_loading
+    @@provider_loading
+  end
+  def self.provider= provider
+    @@provider = provider
+  end
+end
+
 describe Merb::Global::Providers do
+  describe '.provider_name' do
+    it 'should return gettext as default' do
+      Merb::Plugins.expects(:config).returns({})
+      Merb::Global::Providers.provider_name.call.should == 'gettext'
+    end
+    it 'should return the name of the provider in config' do
+      provider = mock
+      config = {:merb_global => {:provider => provider}}
+      Merb::Plugins.expects(:config).returns(config).at_least_once
+      Merb::Global::Providers.provider_name.call.should == provider
+    end
+  end
+  describe '.provider_loading' do
+    it 'should change the provider into correct form' do
+      provider = mock do |provider|
+        provider.expects(:gsub).with(/_/, '').returns("test")
+        provider.stubs(:camel_case).returns("Test")
+      end
+      provider_path = 'merb_global/providers/test'
+      Merb::Global::Providers.expects(:require).with(provider_path)
+      Merb::Global::Providers.stubs(:eval)
+      Merb::Global::Providers.provider_loading.call(provider)
+    end
+    it 'should load the provider' do
+      provider = mock do |provider|
+        provider.stubs(:gsub).with(/_/, '').returns("test")
+        provider.expects(:camel_case).returns("Test")
+      end
+      provider_class = 'Merb::Global::Providers::Test'
+      Merb::Global::Providers.stubs(:require)
+      Merb::Global::Providers.expects(:eval).with(provider_class + '.new')
+      Merb::Global::Providers.provider_loading.call(provider)
+    end
+  end
+  describe '.provider' do
+    before do
+      @provider = Merb::Global::Providers.provider
+    end
+    after do
+      Merb::Global::Providers.provider = @provider
+    end
+    it 'should return the provider' do
+      provider = mock
+      Merb::Global::Providers.provider = provider
+      Merb::Global::Providers.provider.should == provider
+    end
+  end
   describe '.localedir' do
     it 'should return app/locale by default' do
       Merb::Plugins.stubs(:config).returns({})
