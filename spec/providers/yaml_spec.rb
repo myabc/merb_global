eab5a2617f103f7082df4b6ffd969ec1eb6108de
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-17 20:30:43 +0200

    Changed mock-based YAML spec into a file-based spec

--- spec/providers/yaml_spec.rb
+++ spec/providers/yaml_spec.rb
@@ -11,47 +11,22 @@ describe Merb::Global::Providers::Yaml do
   end
   describe '._support?' do
     it 'should return true if file exists' do
-      file = File.join Merb::Global::Providers.localedir, 'en.yaml'
-      File.expects(:exist?).with(file).returns(true)
-      YAML.stubs(:load_file).returns(mock)
-      @provider.support?('en').should == true
+      @provider.support?('pl').should == true
     end
     it 'should return false if file doesn\'t exist' do
-      file = File.join Merb::Global::Providers.localedir, 'en.yaml'
-      File.expects(:exist?).with(file).returns(false)
-      YAML.stubs(:load_file).returns(mock)
-      @provider.support?('en').should == false
+      @provider.support?('fr').should == false
     end
   end
   describe '.translate_to' do
-    it 'should load file if file exists' do
-      yaml_file = File.join Merb::Global::Providers.localedir, 'en.yaml'
-      File.expects(:exist?).with(yaml_file).returns(true)
-      YAML.expects(:load_file).with(yaml_file).returns(nil)
-      @provider.translate_to "test", "tests", :lang => 'en', :n => 1
-    end
     it 'should mark nil if file do not exists' do
-      yaml_file = File.join Merb::Global::Providers.localedir, 'en.yaml'
-      File.expects(:exist?).with(yaml_file).returns(false)
-      @provider.translate_to "test", "tests", :lang => 'en', :n => 1
-      @provider.lang.should include("en")
-      @provider.lang["en"].should be_nil
+      @provider.translate_to "Test", "Tests", :lang => 'fr', :n => 1
+      @provider.lang.should include("fr")
+      @provider.lang["fr"].should be_nil
     end
     it 'should check appropiete form' do
-      yaml_file = File.join Merb::Global::Providers.localedir, 'en.yaml'
-      file_content = {
-        :plural => "n==1?1:0",
-        "test" => {
-          0 => "test",
-          1 => "tests"
-        }
-      }
-      File.expects(:exist?).with(yaml_file).returns(true)
-      YAML.expects(:load_file).with(yaml_file).returns(file_content)
-      expected = [1, file_content[:plural]]
-      Merb::Global::Plural.expects(:which_form).with(*expected).returns(1)
-      translated = @provider.translate_to "test", "test", :lang => 'en', :n => 1
-      translated.should == "tests"
+      translated = @provider.translate_to "Test", "Tests",
+                                          :lang => 'pl', :n => 2
+      translated.should == "Testy"
     end
   end
   describe '.create!' do
