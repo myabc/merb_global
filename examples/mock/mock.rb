80e4f2766518647b238ad2d9195f71728520bbfc
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-18 21:58:53 +0200

    Added examples

---
+++ examples/mock/mock.rb
@@ -0,0 +1,28 @@
+# Load the merb_global from current tree
+Gem.clear_paths
+Gem.path.unshift Pathname(__FILE__).dirname.parent.parent.expand_path + 'lib'
+$LOAD_PATH.unshift Pathname(__FILE__).dirname.parent.parent.expand_path + 'lib'
+
+module Merb::Plugins
+  def self.config
+    {:merb_global => {:provider => :mock}}
+  end
+end
+
+dependency 'merb_global'
+
+Merb::Router.prepare do |r|
+  r.match('/').to(:controller => 'mock', :action =>'index')
+end
+
+class Mock < Merb::Controller
+  def index
+    _("Hi! Hello world!")
+  end
+end
+
+Merb::Config.use { |c|
+  c[:framework]           = {},
+  c[:session_store]       = 'none',
+  c[:exception_details]   = true
+}
