a02f9f614100c151636d5467b99e49b0661ee919
Maciej Piechotka <uzytkownik2@gmail.com>
2008-05-28 12:12:12 +0200

    Updated documentation

--- lib/merb_global/config.rb
+++ lib/merb_global/config.rb
@@ -1,5 +1,22 @@
 module Merb
   module Global
+    # call-seq:
+    #    config(key)                        => value
+    #    config([key1, key2, ...])          => value
+    #    config(key, default)               => value
+    #    config([key1, key2, ...], default) => value
+    #
+    # Lookup the configuration
+    # ==== Params
+    # key<Symbol>:: A key
+    # keys<Array[Symbol]>:: Keys
+    # default<Object>:: A default value
+    #
+    # ==== Returns
+    # value<Object>:: Object read from configuration or default
+    #
+    # ==== Examples
+    # <tt>Merb::Global.config [:gettext, :domain], 'merbapp'</tt>
     def self.config(keys, default = nil)
       keys = [keys] unless keys.is_a? Array
       current = Merb::Plugins.config[:merb_global]
