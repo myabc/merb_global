e4682857eb19f1a2999dc2e8b4817b2beb2f70ce
Maciej Piechotka <uzytkownik2@gmail.com>
2008-06-02 01:11:31 +0200

    Updated loading of parser

--- lib/merb_global/plural.rb
+++ lib/merb_global/plural.rb
@@ -3,8 +3,8 @@ require 'treetop'
 module Merb
   module Global
     module Plural
-      path = Pathname.new(__FILE__).dirname.expand_path + '/plural'
-      @parser = Treetop.load(path).new
+      path = (Pathname.new(__FILE__).dirname.expand_path + 'plural')
+      @parser = Treetop.load(path.to_s).new
 
       # Returns which form should be returned
       # ==== Parameters
