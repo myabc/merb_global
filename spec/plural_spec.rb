123fa7cb5554f12aaabb4e6b3af968e0245a58d1
Maciej Piechotka <uzytkownik2@gmail.com>
2008-06-04 01:28:47 +0200

    Small update of plural spec

--- spec/plural_spec.rb
+++ spec/plural_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
+module Merb::Global::Plural
+  def self.parser
+    @parser
+  end
+end
+
 describe Merb::Global::Plural do
   describe '.which_form' do
-    it 'should call eval' do
-      plural = 'n'
-      Merb::Global::Plural.expects(:eval).with(plural)
-      Merb::Global::Plural.which_form mock, plural
+    it 'should parse the plural' do
+      plural = mock
+      n = mock
+      result = mock
+      tree = mock
+      _lambda = mock
+      _lambda.expects(:call).with(n).returns(result)
+      tree.expects(:to_lambda).returns(_lambda)
+      Merb::Global::Plural.parser.expects(:parse).with(plural).returns(tree)
+      Merb::Global::Plural.which_form(n, plural).should == result
     end
   end
 end
