4084db515ff305649583ef2a2d1b901b1e3bf9b9
Pavel Kunc <pavel@fry-it.com>
2008-06-19 14:01:57 +0100

    Load correct file with extra rake tasks

--- lib/merb_global.rb
+++ lib/merb_global.rb
@@ -4,5 +4,5 @@ if defined? Merb::Plugins
   require Pathname(__FILE__).dirname.expand_path + 'merb_global/base'
   require Pathname(__FILE__).dirname.expand_path + 'merb_global/controller'
 
-  Merb::Plugins.add_rakefiles 'merb_global/rake'
+  Merb::Plugins.add_rakefiles(Pathname(__FILE__).dirname.expand_path + 'merb_global/merbrake')
 end
