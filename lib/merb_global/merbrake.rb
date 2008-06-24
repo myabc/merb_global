a8ded64bec632a42a25943b37e389dc6c4d208c4
Pavel Kunc <pavel.kunc@gmail.com>
2008-06-24 13:58:14 +0100

    Fixed loop over the glob of *.po files

--- lib/merb_global/merbrake.rb
+++ lib/merb_global/merbrake.rb
@@ -1,20 +1,23 @@
 require 'fileutils'
 
 namespace :merb_global do
+  
   task :merb_start do
     Merb.start_environment :adapter => 'runner',
                            :environment => ENV['MERB_ENV'] || 'development'
   end
+  
   desc 'Create migration'
   task :migration => :merb_start do
     Merb::Global::Providers.provider.create!
   end
+  
   desc 'Transfer the translations from one provider to another'
   task :transfer => :merb_start do
     from = Merb::Global.config 'source', 'gettext'
     into = Merb::Global.config 'provider', 'gettext'
     if from == 'gettext' and into == 'gettext'
-      Dir[Merb::Global::Providers.localedir + '/*.po'] do |file|
+      Dir[Merb::Global::Providers.localedir + '/*.po'].each do |file|
         lang = File.basename file, '.po'
         lang_dir = File.join(Merb::Global::Providers.localedir,
                              lang, 'LC_MESSAGES')
@@ -30,4 +33,5 @@ namespace :merb_global do
       Merb::Global::Provider.transfer from, into
     end
   end
+  
 end
