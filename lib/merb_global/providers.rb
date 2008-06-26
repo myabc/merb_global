fc14adbf263f32674a2f9e3db33787eaadd88fe4
Maciej Piechotka <uzytkownik2@gmail.com>
2008-06-26 12:28:38 +0200

    Fixed #24

--- lib/merb_global/providers.rb
+++ lib/merb_global/providers.rb
@@ -4,6 +4,7 @@ module Merb
   module Global
     module Providers
       @@providers = {}
+      @@providers_classes = {}
       ##
       # Creates a provider and/or returns already created one
       #
@@ -14,9 +15,14 @@ module Merb
       # provider<Provider>:: A new provider
       def self.[](provider)
         unless @@providers.include? provider.to_sym
-          require 'merb_global/providers/' + provider
-          klass = "Merb::Global::Providers::#{provider.camel_case}"
-          @@providers[provider.to_sym] = eval "#{klass}.new"
+          if @@providers_classes[provider.to_sym]
+            @@providers[provider.to_sym] =
+              @@providers_classes[provider.to_sym].new
+          else
+            require 'merb_global/providers/' + provider
+            klass = "Merb::Global::Providers::#{provider.camel_case}"
+            @@providers[provider.to_sym] = eval "#{klass}.new"
+          end
         end
         @@providers[provider.to_sym]
       end
@@ -48,6 +54,36 @@ module Merb
       # provider<Provider>:: Returns provider
       def self.provider
         @@provider ||= self[Merb::Global.config(:provider, 'gettext')]
+      end
+
+      # Registers the class under the given name
+      # 
+      # ==== Parameters
+      # name<~to_sym>:: Name under which it is registered
+      # klass<Class>:: Class of the provider
+      def self.register(name, klass)
+        @@provider_classes[name.to_sym] = klass
+      end
+
+      # Perform the registration
+      #
+      # ==== Parameters
+      # name<~to_sym>:: Name under which it is registred
+      # opts<Array[Symbol]>:: Additional imformations
+      #
+      # ==== Options
+      # importer:: Can perform import
+      # exporter:: Can perform export
+      def self.Provider(name, *opts)
+        Module.new do
+          include Base
+          include Base::Importer if opts.include? :importer
+          include Base::Exporter if opts.include? :exporter
+          
+          def self.included(klass)
+            Merb::Global::Providers.register name, klass
+          end
+        end
       end
     end
   end
