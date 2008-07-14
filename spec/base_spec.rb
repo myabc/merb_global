7bf6ac673363d89b474389679ef9ae227ae6c656
Maciej Piechotka <uzytkownik2@gmail.com>
2008-07-15 00:08:02 +0200

    100% coverage of Base

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -43,36 +43,94 @@ describe Merb::Global do
   
 
   describe '._' do
-    it 'should send singular and nil if plural not given' do
-      test_base = TestBase.new
-      test_base.message_provider = mock do |provider|
-        expected_args = ['a', nil, {:n => 1, :lang => 'en'}]
-        provider.expects(:translate_to).with(*expected_args).returns('b')
+    describe 'message' do
+      it 'should send singular and nil if plural not given' do
+        test_base = TestBase.new
+        test_base.message_provider = mock do |provider|
+          expected_args = ['a', nil, {:n => 1, :lang => 'en'}]
+          provider.expects(:translate_to).with(*expected_args).returns('b')
+        end
+        test_base._('a').should == 'b'
       end
-      test_base._('a').should == 'b'
-    end
-
-    it 'should send singular and plural if both given' do
-      test_base = TestBase.new
-      test_base.message_provider = mock do |provider|
-        expected_args = ['a', 'b', {:n => 1, :lang => 'en'}]
-        provider.expects(:translate_to).with(*expected_args).returns('a')
+      
+      it 'should send singular and plural if both given' do
+        test_base = TestBase.new
+        test_base.message_provider = mock do |provider|
+          expected_args = ['a', 'b', {:n => 1, :lang => 'en'}]
+          provider.expects(:translate_to).with(*expected_args).returns('a')
+        end
+        test_base._('a', 'b').should == 'a'
       end
-      test_base._('a', 'b').should == 'a'
-    end
-
-    it 'should send the proper number if given' do
-      test_base = TestBase.new
-      test_base.message_provider = mock do |provider|
-        expected_args = ['a', 'b', {:n => 2, :lang => 'en'}]
-        provider.expects(:translate_to).with(*expected_args).returns('b')
+      
+      it 'should send the proper number if given' do
+        test_base = TestBase.new
+        test_base.message_provider = mock do |provider|
+          expected_args = ['a', 'b', {:n => 2, :lang => 'en'}]
+          provider.expects(:translate_to).with(*expected_args).returns('b')
+        end
+        test_base._('a', 'b', :n => 2).should == 'b'
       end
-      test_base._('a', 'b', :n => 2).should == 'b'
+      
+      it 'should raise ArgumentException for wrong number of arguments' do
+        lambda {TestBase.new._ 'a', 'b', 'c'}.should raise_error(ArgumentError)
+      end
     end
+    
+    describe 'with date' do
+      it 'should send the date and format' do
+        test_base = TestBase.new
+        time = Time.new
+        result = mock
+        test_base.date_provider = mock do |provider|
+          expected_args = ['en', time, '%A']
+          provider.expects(:localize).with(*expected_args).returns(result)
+        end
+        test_base._(time, '%A').should == result
+      end
 
-    it 'should raise ArgumentException for wrong number of arguments' do
+      it 'should send the date, format and language if given' do
+        test_base = TestBase.new
+        time = Time.new
+        result = mock
+        test_base.date_provider = mock do |provider|
+          expected_args = ['fr', time, '%A']
+          provider.expects(:localize).with(*expected_args).returns(result)
+        end
+        test_base._(time, '%A', :lang => 'fr').should == result
+      end
+      
+      it 'should raise an error on wrong arguments' do
+        time = Time.new
+        lambda {TestBase.new._(time)}.should raise_error(ArgumentError)
+        lambda {TestBase.new._(time, '', 1)}.should raise_error(ArgumentError)
+      end
+    end
+    
+    describe 'with number' do
+      it 'should send the number' do
+        test_base = TestBase.new
+        test_base.numeric_provider = mock do |provider|
+          expected_args = ['en', 1.0]
+          provider.expects(:localize).with(*expected_args).returns('1,0')
+        end
+        test_base._(1.0).should == '1,0'
+      end
+      
+      it 'should send the number and language if given' do
+        test_base = TestBase.new
+        test_base.numeric_provider = mock do |provider|
+          expected_args = ['pl', 1.0]
+          provider.expects(:localize).with(*expected_args).returns('1,0')
+        end
+        test_base._(1.0, :lang => 'pl').should == '1,0'
+      end
+
+      it 'should raise an error on wrong arguments' do
+        lambda {TestBase.new._(1.0, 1)}.should raise_error(ArgumentError)
+      end
+    end
+    it 'should raise an error on no argument' do
       lambda {TestBase.new._}.should raise_error(ArgumentError)
-      lambda {TestBase.new._ 'a', 'b', 'c'}.should raise_error(ArgumentError)
     end
   end
 end
