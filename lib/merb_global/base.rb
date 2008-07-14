8163945fdb20ae38b7dfe3d5b7407245ad38ec93
Maciej Piechotka <uzytkownik2@gmail.com>
2008-07-14 11:51:10 +0200

    Added other providers to base module

--- lib/merb_global/base.rb
+++ lib/merb_global/base.rb
@@ -4,14 +4,22 @@ require 'merb_global/message_providers'
 
 module Merb
   module Global
-    attr_accessor :lang, :provider
+    attr_accessor :lang, :message_provider, :date_provider, :numeric_provider
 
     def lang #:nodoc:
       @lang ||= 'en'
     end
 
     def message_provider #:nodoc:
-      @provider ||= Merb::Global::MessageProviders.provider
+      @message_provider ||= Merb::Global::MessageProviders.provider
+    end
+
+    def date_provider #:nodoc:
+      @date_provider ||= Merb::Global::DateProvider.provider
+    end
+
+    def numeric_provider #:nodoc:
+      @numeric_provider ||= Merb::Global::NumericProvider.provider
     end
 
     # call-seq:
@@ -37,9 +45,9 @@ module Merb
       opts = {:lang => self.lang, :n => 1}
       opts.merge! args.pop if args.last.is_a? Hash
       if args.size == 1
-        self.provider.translate_to args[0], nil, opts
+        self.message_provider.translate_to args[0], nil, opts
       elsif args.size == 2
-        self.provider.translate_to args[0], args[1], opts
+        self.message_provider.translate_to args[0], args[1], opts
       else
         raise ArgumentError, "wrong number of arguments (#{args.size} for 1-2)"
       end
