06be09924f8d75d1ade5d8b6e166b9f33579e742
Maciej Piechotka <uzytkownik2@gmail.com>
2008-07-14 12:00:50 +0200

    Simple localization API for date and numerics as well

--- lib/merb_global/base.rb
+++ lib/merb_global/base.rb
@@ -23,18 +23,23 @@ module Merb
     end
 
     # call-seq:
-    #   _(singular, opts)          => translated
-    #   _(singlular, plural, opts) => translated
+    #   _(singular, opts)          => translated message
+    #   _(singlular, plural, opts) => translated message
+    #   _(date, format)            => localized date
+    #   _(number)                  => localized number
     #
     # Translate a string.
     # ==== Parameters
     # singular<String>:: A string to translate
     # plural<String>:: A plural form of string
     # opts<Hash>:: An options hash (see below)
+    # date<~strftime>:: A date to localize
+    # format<String>:: A format of string (should be compatibile with strftime)
+    # number<Numeric>:: A numeber to localize
     #
     # ==== Options (opts)
     # :lang<String>:: A language to translate on
-    # :n<Fixnum>:: A number of objects
+    # :n<Fixnum>:: A number of objects (for messages)
     #
     # ==== Returns
     # translated<String>:: A translated string
@@ -44,12 +49,30 @@ module Merb
     def _(*args)
       opts = {:lang => self.lang, :n => 1}
       opts.merge! args.pop if args.last.is_a? Hash
-      if args.size == 1
-        self.message_provider.translate_to args[0], nil, opts
-      elsif args.size == 2
-        self.message_provider.translate_to args[0], args[1], opts
+      if args.first.respond_to? :strftime
+        if args.size == 2
+          self.date_provider.localize opts[:lang], args[0], args[1]
+        else
+          raise ArgumentError, "wrong number of arguments (#{args.size} for 2)"
+        end
+      elsif args.first.is_a? Numeric
+        if args.size == 1
+          self.numeric_provider.localize opts[:lang], args.first
+        else
+          raise ArgumentError, "wrong number of arguments (#{args.size} for 1)"
+        end
+      elsif args.first.is_a? String
+        if args.size == 1
+          self.message_provider.translate_to args[0], nil, opts
+        elsif args.size == 2
+          self.message_provider.translate_to args[0], args[1], opts
+        else
+          raise ArgumentError,
+                "wrong number of arguments (#{args.size} for 1-2)"
+        end
       else
-        raise ArgumentError, "wrong number of arguments (#{args.size} for 1-2)"
+        raise ArgumentError,
+              "wrong type of arguments - see documentation for details"
       end
     end
   end
